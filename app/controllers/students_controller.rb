6113b34f1f177821f528f6bab9d81f6a12caf02f
Matt Williams <matt.seongjun@gmail.com>
2019-06-10 10:51:01 -0400

    Add create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,22 @@
 class StudentsController < ApplicationController
-  
-  def index
-    @students = Student.all
-  end
+    
+    def index
+        @students = Student.all
+    end
 
-  def show
-    @student = Student.find(params[:id])
-  end
+    def show
+        @student = Student.find(params[:id])
+    end
 
-  def new
-  end
+    def new
+    end
 
-  def create
-  end
+    def create
+        @student = Student.create(
+            first_name: params[:first_name],
+            last_name: params[:last_name]
+        )
+        redirect_to students_path(@student)
+    end
 
 end
